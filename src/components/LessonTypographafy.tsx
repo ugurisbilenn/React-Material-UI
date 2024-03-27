a48cd31f202f48b48665d03f9f936eab2ccf542b
Ugur İsbilen <ugur.isbilen41@gmail.com>
2024-03-27 21:59:43 +0300

    Typoghraphy

---
+++ src/components/LessonTypographafy.tsx
@@ -0,0 +1,32 @@
+import { Typography } from '@mui/material';
+
+
+export const LessonTypographafy = () => {
+  return (
+    <div>
+
+        <Typography variant='body1'>
+        Body Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod quas dignissimos quaerat in! Molestiae inventore neque labore vero eveniet. Suscipit esse, a fuga accusamus optio voluptas necessitatibus tempora facilis!
+
+        </Typography>
+        <Typography variant='body2'>
+        Body Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quod quas dignissimos quaerat in! Molestiae inventore neque labore vero eveniet. Suscipit esse, a fuga accusamus optio voluptas necessitatibus tempora facilis!
+        
+        </Typography>
+        <Typography variant='h1'>H1 Görünüm</Typography>
+        <Typography variant='h2'>H2 Görünüm</Typography>
+        <Typography variant='h3'>H3 Görünüm</Typography>
+        <Typography variant='h4'>H4 Görünüm</Typography>
+        <Typography variant='h5'>H5 Görünüm</Typography>
+        <Typography variant='h6'>H6 Görünüm</Typography>
+
+        <Typography variant='h1' component='h4'>H1 Görünüm Component</Typography>
+
+        <Typography variant='h1' component='h4' align='left'>H1 Görünüm Component</Typography>
+        <Typography variant='h1' component='h4' align='right'>H1 Görünüm Component</Typography>
+
+        <Typography variant='subtitle1'>H6 Görünüm</Typography>
+        <Typography variant='subtitle2'>H6 Görünüm</Typography>
+    </div>
+  )
+}
