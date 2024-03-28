ea0abacc9474514e1ee80a65d59c2100993a6ec0
Ugur İsbilen <ugur.isbilen41@gmail.com>
2024-03-28 22:35:31 +0300

    Multiple Select

--- src/components/LessonSelect.tsx
+++ src/components/LessonSelect.tsx
@@ -2,15 +2,29 @@ import {Box,TextField,MenuItem} from '@mui/material';
 import {useState} from 'react';
 
 export const LessonSelect = () => {
-    const [country, setCountry] = useState('');
+    const [country, setCountry] = useState<string[]>([]);
     // const handleChange = () => {
 
     // }
+    console.log(country);
   return (
     <Box width="250px">
-        <TextField  label="Ülke Seçiniz" select fullWidth
+        {/* <TextField  label="Ülke Seçiniz" select fullWidth
         value={country}
         onChange={(e) => setCountry(e.target.value)}
+        >
+            <MenuItem value="TR">Türkiye</MenuItem>
+            <MenuItem value="USA">ABD</MenuItem>
+            <MenuItem value="FR">FRANCE</MenuItem>
+        </TextField> */}
+
+        <TextField  label="Ülke Seçiniz" 
+        SelectProps={{
+            multiple:true,
+        }}
+        select fullWidth
+        value={country}
+        onChange={(e) => setCountry(typeof e.target.value === 'string' ? e.target.value.split(',') : e.target.value)}
         >
             <MenuItem value="TR">Türkiye</MenuItem>
             <MenuItem value="USA">ABD</MenuItem>
